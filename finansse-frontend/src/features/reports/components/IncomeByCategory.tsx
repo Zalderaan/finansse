ca316d6a163a70996c370c47272ac1cf51eb87df
Godfrey Eclarinal <godfreyeclarinal21@gmail.com>
2025-12-08 13:42:58 +0800

    reset empty states for dashb pie charts

--- finansse-frontend/src/features/reports/components/IncomeByCategory.tsx
+++ finansse-frontend/src/features/reports/components/IncomeByCategory.tsx
@@ -26,39 +26,27 @@ export function IncomeByCategory() {
                     Your income
                 </CardDescription>
             </CardHeader>
-            {
-                !userIncomeByCategory || userIncomeByCategory.length === 0 ? (
-                    <CardContent className="flex flex-col items-center justify-center h-64 space-y-4">
-                        <TrendingUp className="w-12 h-12 text-gray-400" />
-                        <div className="text-center">
-                            <p className="font-semibold text-gray-700">No income data yet</p>
-                            <p className="text-sm text-gray-500">Start adding income entries to see your breakdown.</p>
-                        </div>
-                        <Button variant="outline">Add Income</Button> {/* Link to add income page */}
-                    </CardContent>
-                ) : (
-                    <CardContent>
-                        <PieChart width={300} height={300}>
-                            <Pie
-                                data={userIncomeByCategory}
-                                dataKey="total_amount"
-                                nameKey="category_name"
-                                innerRadius={60}
-                                outerRadius={80}
-                                cornerRadius={5}
-                                paddingAngle={5}
-                            >
 
-                                {userIncomeByCategory?.map((_, index) => (
-                                    <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-                                ))}
-                            </Pie>
-                            <Tooltip formatter={(value: number) => `${value.toLocaleString("en-PH", { style: "currency", currency: "PHP" })}`} />
-                            <Legend />
-                        </PieChart>
-                    </CardContent>
-                )
-            }
+            <CardContent>
+                <PieChart width={300} height={300}>
+                    <Pie
+                        data={userIncomeByCategory}
+                        dataKey="total_amount"
+                        nameKey="category_name"
+                        innerRadius={60}
+                        outerRadius={80}
+                        cornerRadius={5}
+                        paddingAngle={5}
+                    >
+
+                        {userIncomeByCategory?.map((_, index) => (
+                            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                        ))}
+                    </Pie>
+                    <Tooltip formatter={(value: number) => `${value.toLocaleString("en-PH", { style: "currency", currency: "PHP" })}`} />
+                    <Legend />
+                </PieChart>
+            </CardContent>
         </Card>
     )
 }
--- finansse-frontend/src/features/reports/components/SpendingByCategory.tsx
+++ finansse-frontend/src/features/reports/components/SpendingByCategory.tsx
@@ -33,50 +33,26 @@ export function SpendingByCategory() {
                     Your spending
                 </CardDescription>
             </CardHeader>
-            {!userSpendByCategory || userSpendByCategory.length === 0 ? (
-                <CardContent className="flex flex-col items-center justify-center h-64 space-y-4">
-                    <DollarSign className="w-12 h-12 text-gray-400" />
-                    <div className="text-center">
-                        <p className="font-semibold text-gray-700">
-                            {hasAccounts ? "No spending data yet" : "No accounts found"}
-                        </p>
-                        <p className="text-sm text-gray-500">
-                            {hasAccounts
-                                ? "Start adding expense entries to see your breakdown."
-                                : "Create an account to start tracking spending."
-                            }
-                        </p>
-                    </div>
-                    <Button variant="outline" asChild>
-                        {hasAccounts
-                            ? (<AddTransactionDialog width="fit" className="">
-                                Add Expense
-                            </AddTransactionDialog>)
-                            : (<Link to={"/dashboard/accounts"}>Create account</Link>)}
-                    </Button>
-                </CardContent>
-            ) : (
-                <CardContent>
-                    <PieChart width={300} height={300}>
-                        <Pie
-                            data={userSpendByCategory}
-                            dataKey="total_amount"
-                            nameKey="category_name"
-                            innerRadius={60}
-                            outerRadius={80}
-                            cornerRadius={5}
-                            paddingAngle={5}
-                        >
-                            {userSpendByCategory?.map((_, index) => (
-                                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-                            ))}
-                        </Pie>
-                        <Tooltip formatter={(value: number) => `${value.toLocaleString("en-PH", { style: "currency", currency: "PHP" })}`} />
-                        <Legend />
-                    </PieChart >
-                </CardContent >
-            )
-            }
+
+            <CardContent>
+                <PieChart width={300} height={300}>
+                    <Pie
+                        data={userSpendByCategory}
+                        dataKey="total_amount"
+                        nameKey="category_name"
+                        innerRadius={60}
+                        outerRadius={80}
+                        cornerRadius={5}
+                        paddingAngle={5}
+                    >
+                        {userSpendByCategory?.map((_, index) => (
+                            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                        ))}
+                    </Pie>
+                    <Tooltip formatter={(value: number) => `${value.toLocaleString("en-PH", { style: "currency", currency: "PHP" })}`} />
+                    <Legend />
+                </PieChart >
+            </CardContent >
         </Card >
     )
 }
