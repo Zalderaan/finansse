f792367e6bdb306fd750c7091bee400a952cebf0
Godfrey Eclarinal <godfreyeclarinal21@gmail.com>
2026-02-04 23:06:30 +0800

    skeleton in CategoryTable

--- finansse-frontend/src/features/categories/components/CategoryTable.tsx
+++ finansse-frontend/src/features/categories/components/CategoryTable.tsx
@@ -1,10 +1,12 @@
 import { DataTable } from "@/components/ui/data-table"; // Assuming this is your shadcn DataTable
-// import { Skeleton } from "@/components/ui/skeleton";
+import { Skeleton } from "@/components/ui/skeleton";
+import type { Category } from "../types/categories.types";
+import type { ColumnDef } from "@tanstack/table-core";
 // import { Loader2 } from "lucide-react";
 
 interface CategoryTableProps {
-    columns: any; // Replace with proper types
-    data: any[];
+    columns: ColumnDef<Category>[]; // Replace with proper types
+    data: Category[];
     isLoading: boolean;
     isError: boolean;
     error: Error | null;
@@ -12,20 +14,20 @@ interface CategoryTableProps {
 
 export function CategoryTable({ columns, data, isLoading, isError, error }: CategoryTableProps) {
     // isLoading = true;
-    // if (isLoading) {
-    //     return (
-    //         <div className="space-y-4">
-    //             {/* Render skeleton rows to match table structure */}
-    //             {Array.from({ length: 5 }).map((_, index) => (  // Adjust length for desired number of skeleton rows
-    //                 <div key={index} className="flex space-x-4">
-    //                     {columns.map((col: string, colIndex: number) => (
-    //                         <Skeleton key={colIndex} className="h-4 w-full" />  // Adjust width/class for column-specific sizing
-    //                     ))}
-    //                 </div>
-    //             ))}
-    //         </div>
-    //     );
-    // }
+    if (isLoading) {
+        return (
+            <div className="space-y-4">
+                {/* Render skeleton rows to match table structure */}
+                {Array.from({ length: 5 }).map((_, index) => (  // Adjust length for desired number of skeleton rows
+                    <div key={index} className="flex space-x-4">
+                        {columns.map((col, colIndex: number) => (
+                            <Skeleton key={colIndex} className={`h-4 ${col.size ? `w-[${col.size}px]` : 'w-full'}`} />
+                        ))}
+                    </div>
+                ))}
+            </div>
+        );
+    }
 
     if (isError) {
         return (
