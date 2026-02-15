fb48a0de5416fc640eb41375ebdbbf4308c19765
Godfrey Eclarinal <godfreyeclarinal21@gmail.com>
2026-02-16 00:40:33 +0800

    jwt small fix

--- finansse-backend/src/utils/jwt.ts
+++ finansse-backend/src/utils/jwt.ts
@@ -6,9 +6,9 @@ export interface TokenPair {
 }
 
 export class jwtUtil {
-    private static readonly SECRET_KEY = process.env.JWT_SECRET || 'secret';
-    private static readonly token_expiry = '30m';
-    private static readonly refresh_expiry = '7d';
+    private static readonly SECRET_KEY = process.env.JWT_SECRET || 'finansse_jwt_secret';
+    private static readonly token_expiry: string = process.env.JWT_EXPIRES_IN  || '30m';
+    private static readonly refresh_expiry: string = '7d';
     // private static readonly token_expiry = '1m';
     // private static readonly refresh_expiry = '1m';
 
@@ -23,9 +23,12 @@ export class jwtUtil {
     }
 
     static generateAccessToken(payload: object): string {
+        // const accessToken = jwt.sign(payload, this.SECRET_KEY, {
+        //     expiresIn: this.token_expiry
+        // });
         const accessToken = jwt.sign(payload, this.SECRET_KEY, {
-            expiresIn: this.token_expiry
-        });
+            expiresIn: '30m' // ! hardcoded... using token_expiry has some errors
+        })
         return accessToken;
     }
 
@@ -44,7 +47,7 @@ export class jwtUtil {
 
     static generateRefreshToken(payload: object): string {
         return jwt.sign(payload, this.SECRET_KEY, {
-            expiresIn: this.refresh_expiry
+            expiresIn: '7d' // ! hardcoded... using refresh_expiry has some errors
         });
     }
 
