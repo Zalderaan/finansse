79d09817e22c8e13010667e0bb112981d5c2e8e0
Godfrey Eclarinal <godfreyeclarinal21@gmail.com>
2026-02-16 00:44:44 +0800

    small bugfix in reports model

--- finansse-backend/src/models/reports.model.ts
+++ finansse-backend/src/models/reports.model.ts
@@ -1,5 +1,5 @@
 import prisma from "../db";
-import { Prisma } from "@prisma/client";
+import { Prisma } from '../generated/prisma/client';
 
 export class BalanceModel {
     static async fetchUserBalanceTrend(userId: number, period: string) {
