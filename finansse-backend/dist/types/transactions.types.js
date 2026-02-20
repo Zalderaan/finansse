67687a3c20c5e655880893f0aaf36639368802ec
Godfrey Eclarinal <godfreyeclarinal21@gmail.com>
2026-02-21 01:04:23 +0800

    modified gitignore

--- finansse-backend/dist/types/transactions.types.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// export interface GetTransactionsByAccRequest {
-// }
-//# sourceMappingURL=transactions.types.js.map
