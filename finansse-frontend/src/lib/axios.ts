bc395e1f139b2e101f416124942eef4eb9ad0ce3
Godfrey Eclarinal <godfreyeclarinal21@gmail.com>
2026-02-22 01:49:36 +0800

    added cors allowed origins

--- finansse-frontend/src/lib/axios.ts
+++ finansse-frontend/src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { setupInterceptors } from './axios.interceptors';
 
 const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001/finansse-backend';
-
+console.log("This is API_BASE_URL: ", API_BASE_URL)
 // create axios instance
 export const axiosInstance = axios.create({
     baseURL: API_BASE_URL,
